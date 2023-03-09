373a6d3ad223360e52809be3e6d6e4def094e260
Victor Garcia Diaz <127221758+vgarcia299@users.noreply.github.com>
2023-03-09 22:23:15 +0100

    Update CheckedException.java

--- misTools/CheckedException.java
+++ misTools/CheckedException.java
@@ -3,12 +3,12 @@ package misTools;
  * Excepcion generica que salta cuando algo se hace de una forma indeseada.
  * @author victor
  */
-public class CheckedException extends Exception {
+public class CustomException extends Exception {
 	private static final long serialVersionUID = 34390014111499493L;
-	public CheckedException() {
+	public CustomException() {
 		super("Excepción Genérica. Probablemente haya saltado x fallo lógico.");
 	}
-	public CheckedException(String descripcion) {
+	public CustomException(String descripcion) {
 		super("descripcion");
 	}
 }
